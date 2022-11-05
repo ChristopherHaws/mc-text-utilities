ecba79e3d296dd8ef7378691b14e03c5251d4464
Christopher Haws <christopher.haws@arvato-scs.com>
2022-11-05 03:30:35 -0700

    Updated UI

--- src/main/java/dev/chaws/textutilities/handlers/FormatButtonsHandler.java
+++ src/main/java/dev/chaws/textutilities/handlers/FormatButtonsHandler.java
@@ -15,6 +15,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FormatButtonsHandler {
+	private final static List<Formatting> colorFormattings = List.of(
+		Formatting.BLACK,
+		Formatting.DARK_GRAY,
+		Formatting.DARK_BLUE,
+		Formatting.BLUE,
+		Formatting.DARK_GREEN,
+		Formatting.GREEN,
+		Formatting.DARK_AQUA,
+		Formatting.AQUA,
+		Formatting.DARK_RED,
+		Formatting.RED,
+		Formatting.DARK_PURPLE,
+		Formatting.LIGHT_PURPLE,
+		Formatting.GOLD,
+		Formatting.YELLOW,
+		Formatting.GRAY,
+		Formatting.WHITE
+	);
+
+	private final static List<Formatting> modifierFormattings = List.of(
+		Formatting.BOLD,
+		Formatting.ITALIC,
+		Formatting.UNDERLINE,
+		Formatting.STRIKETHROUGH,
+		Formatting.OBFUSCATED,
+		Formatting.RESET
+	);
+
 	public static void initialize() {
 		ScreenEvents.AFTER_INIT.register((client, screen, width, height) ->
 			onScreenOpened(screen)
@@ -22,63 +50,107 @@ public class FormatButtonsHandler {
 	}
 
 	private static void onScreenOpened(Screen screen) {
-		if (!(screen instanceof SignEditScreen) &&
-			!(screen instanceof BookEditScreen) &&
-			!(screen instanceof AnvilScreen)) {
-			return;
-		}
+		var offsetFromCenter = 0;
 
-		if (screen instanceof AnvilScreen anvilScreen) {
+		if (screen instanceof SignEditScreen) {
+			offsetFromCenter = 50;
+		} else if (screen instanceof BookEditScreen) {
+			offsetFromCenter = 70;
+		} else if (screen instanceof AnvilScreen anvilScreen) {
 			((AnvilScreenAccessor)anvilScreen).getNameField().setRenderTextProvider((abc, def) ->
 				Text.literal(abc).asOrderedText()
 			);
+
+			offsetFromCenter += 85;
+		} else {
+			// Not a supported screen.
+			return;
 		}
 
-		var x = 16 * screen.width / 17;
-		
-		var buttons = getFormatButtons(screen, x);
-		for (var button : buttons) {
-			Screens.getButtons(screen).add(button);
-		}
+		// TODO: Try to use ButtonListWidget
+
+		var colorButtons = getFormatButtons(
+			screen,
+			colorFormattings,
+			screen.width / 2 - (120 + offsetFromCenter),
+			70,
+			4
+		);
+
+		var modifierButtons = getFormatButtons(
+			screen,
+			modifierFormattings,
+			screen.width / 2 + offsetFromCenter,
+			50,
+			6
+		);
+
+		var screenButtons = Screens.getButtons(screen);
+		screenButtons.addAll(colorButtons);
+		screenButtons.addAll(modifierButtons);
 	}
 
-	private static List<ButtonWidget> getFormatButtons(Screen screen, int x) {
+	private static List<ButtonWidget> getFormatButtons(Screen screen, List<Formatting> formats, int x, int yOffset, int rows) {
 		List<ButtonWidget> list = new ArrayList<>();
 		var i = 0;
+		var gap = 0;
+		var buttonSize = 20;
 
-		for (var formatting : Formatting.values()) {
-			var buttonX = x - (i / 11 + 1) * 40;
-			var buttonY = i % 11 * 20 + 15;
+		for (var formatting : formats) {
+			var buttonX = x + (i / rows + 1) * (buttonSize + gap);
+			var buttonY = i % rows * (buttonSize + gap) + yOffset;
 
-			if (formatting == Formatting.RESET) {
-				list.add(new ButtonWidget(
-					buttonX,
-					buttonY,
-					40,
-					20,
-					Text.literal(formatting.toString().concat("Reset")),
-					cod -> {
-						screen.charTyped(Formatting.FORMATTING_CODE_PREFIX, 0);
-						screen.charTyped(formatting.getCode(), 0);
-					}
-				));
-			} else {
-				list.add(new ButtonWidget(
-					buttonX,
-					buttonY,
-					20,
-					20,
-					Text.literal(formatting.toString().concat("A")),
-					cod -> {
-						screen.charTyped(Formatting.FORMATTING_CODE_PREFIX, 0);
-						screen.charTyped(formatting.getCode(), 0);
-					}
-				));
-			}
+			list.add(getFormatButton(
+				screen,
+				buttonX,
+				buttonY,
+				buttonSize,
+				buttonSize,
+				formatting
+			));
 
 			i++;
 		}
 
 		return list;
+	}
+
+	private static ButtonWidget getFormatButton(
+		Screen screen,
+		int buttonX,
+		int buttonY,
+		int buttonWidth,
+		int buttonHeight,
+		Formatting formatting
+	) {
+		if (formatting.isModifier() || formatting == Formatting.RESET) {
+			var label = formatting.toString().concat(formatting.getName());
+			return new ButtonWidget(
+				buttonX,
+				buttonY,
+				buttonWidth * 4,
+				buttonHeight,
+				Text.literal(label),
+				cod -> {
+					screen.charTyped(Formatting.FORMATTING_CODE_PREFIX, 0);
+					screen.charTyped(formatting.getCode(), 0);
+				},
+				(button, matrices, mouseX, mouseY) -> {
+					
+				}
+			);
+		}
+
+		return new ButtonWidget(
+			buttonX,
+			buttonY,
+			buttonWidth,
+			buttonHeight,
+			Text.literal(formatting.toString().concat("⬛")),
+			cod -> {
+				screen.charTyped(Formatting.FORMATTING_CODE_PREFIX, 0);
+				screen.charTyped(formatting.getCode(), 0);
+			}
+		);
 	}
 }
