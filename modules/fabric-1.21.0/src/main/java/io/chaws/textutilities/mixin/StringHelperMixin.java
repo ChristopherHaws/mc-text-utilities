f3b06200e12aeb4cd25a36ee194072f923d9254c
Christopher Haws <ChristopherHaws@users.noreply.github.com>
2024-07-21 17:56:26 -0700

    Fix 1.21 support

--- modules/fabric-1.21.0/src/main/java/io/chaws/textutilities/mixin/StringHelperMixin.java
+++ modules/fabric-1.21.0/src/main/java/io/chaws/textutilities/mixin/StringHelperMixin.java
@@ -1,15 +1,16 @@
 package io.chaws.textutilities.mixin;
 
 import io.chaws.textutilities.TextUtilities;
-import net.minecraft.SharedConstants;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.StringHelper;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SharedConstants.class)
-public class SharedConstantsMixin {
+@Mixin(StringHelper.class)
+public class StringHelperMixin {
 	@Inject(method = "isValidChar", at = @At("HEAD"), cancellable = true)
 	private static void isValidChar(char p, CallbackInfoReturnable<Boolean> ci) {
 		if (TextUtilities.getConfig().formattingDisabled()) {
